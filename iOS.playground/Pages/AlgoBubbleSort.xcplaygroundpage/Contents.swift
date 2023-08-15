5643f8da95ff20c3f2a5ef8e30547df2345647df
sanjay <kochrekarsanjay87@gmail.com>
2023-08-15 10:45:53 +0530

    add bubble sort extension

---
+++ iOS.playground/Pages/AlgoBubbleSort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+extension Array where Element: Comparable {
+    
+    mutating func bubbleSort() {
+        for i in 0...self.count - 1 {
+            for j in (i..<self.count - 1).reversed() {
+                if self[j] > self[j + 1] {
+                    let temp = self[j]
+                    self[j] = self[j + 1]
+                    self[j + 1] = temp
+                }
+            }
+        }
+    }
+}
+
+var array = [5, 1, 2, 6, 4, 7, 4, 4, 3, 1]
+array.bubbleSort()
+print(array)
+
+var arrayStr = ["12", "a", "g", "01", "h", "j","a", "s", "d", "i"]
+arrayStr.bubbleSort()
+print(arrayStr)
